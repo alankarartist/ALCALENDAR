eb892e858ee6a463b5f28789bd3b558f9d65d742
alankarartist <alankarartist@gmail.com>
2022-10-05 22:33:02 +0530

    ALCALENDAR

--- AlCalendar.py
+++ AlCalendar.py
@@ -89,7 +89,8 @@ class AlCalendar():
         titleBar.bind("<Button-3>", showScreen)
         titleBar.bind("<Map>", screenAppear)
 
-        liftWindow()
+        if systemName == 'Windows':
+            liftWindow()
         root.mainloop()
 
 
